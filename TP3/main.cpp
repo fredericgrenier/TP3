cc8bcc6bc7491c1038046f8667e894e3dd40e99a
Félix Vallières Goulet <flx890@gmail.com>
2015-04-06 20:03:03 -0400

    Refactored findDistance Test methode

--- TP3/main.cpp
+++ TP3/main.cpp
@@ -23,12 +23,12 @@ void Two_Way_Test(std::string path, uint32_t from, uint32_t to)
 	start = std::chrono::high_resolution_clock::now();
 	graph.afficher_chemin(graph.trouver_chemin(from, to));
 	end = std::chrono::high_resolution_clock::now();
-	std::cout << "Duration A: " << std::chrono::duration_cast<std::chrono::duration<double>>(end - start).count() << '\n';
+	std::cout << "Duration A: " << std::chrono::duration_cast<std::chrono::duration<double>>(end - start).count() << endl;
 
 	start = std::chrono::high_resolution_clock::now();
 	graph.afficher_chemin(graph.trouver_chemin(to, from));
 	end = std::chrono::high_resolution_clock::now();
-	std::cout << "Duration B: " << std::chrono::duration_cast<std::chrono::duration<double>>(end - start).count() << '\n';
+	std::cout << "Duration B: " << std::chrono::duration_cast<std::chrono::duration<double>>(end - start).count() << endl;
 }
 
 void findDistance(string filePath, uint32_t numreoNoeud, float latitude, float longitude)
@@ -36,15 +36,16 @@ void findDistance(string filePath, uint32_t numreoNoeud, float latitude, float l
 	std::chrono::high_resolution_clock::time_point start;
 	std::chrono::high_resolution_clock::time_point end;
 	graphe G(filePath);
-	cout << "point : " << numreoNoeud << " distance : " << G.distance(numreoNoeud, latitude, longitude) << endl;
+
+	cout << "point : " << numreoNoeud << " distance : " << G.distance(numreoNoeud, latitude, longitude) << endl << endl;
+	start = std::chrono::high_resolution_clock::now();
 	numreoNoeud = G.localiser(latitude, longitude);
-	cout << "point : "
-		<< numreoNoeud
-		<< " distance : "
-		<< G.distance(numreoNoeud, latitude, longitude)
-		<< " noeuds lus : "
-		<< G.size_map()
-		<< endl;
+	end = std::chrono::high_resolution_clock::now();
+	std::cout << "Duree de la localisation du noeud: " << std::chrono::duration_cast<std::chrono::duration<double>>(end - start).count() << endl;
+	cout << "point : " << numreoNoeud << endl
+		 << " distance : " << G.distance(numreoNoeud, latitude, longitude) << endl
+		 << " noeuds lus : " << G.size_map() << endl
+		 << endl;
 }
 
 void askNode()
@@ -70,7 +71,7 @@ int main()
 	//Two_Way_Test("C:\\Tests\\grUSA_32768.ibin", 3489, 6666);
 
 	//findDistance("grUSA_1024.ibin", 841, 40, -70);
-	findDistance("C:\\Tests\\grUSA_1024.ibin", 841, 40, -70);
+	//findDistance("C:\\Tests\\grUSA_1024.ibin", 841, 40, -70);
 	findDistance("C:\\Tests\\grUSA_1048576.ibin", 841, 40, -70);
 
 	return 0;
